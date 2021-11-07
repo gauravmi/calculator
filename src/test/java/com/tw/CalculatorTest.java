f2f4b85d8a66c078936f35e40474863bf7025ef8
Gaurav Ingalkar <gaurav@Gauravs-MacBook-Pro.local>
2021-11-07 20:39:43 +0530

    add calculator test for divide by zero

--- src/test/java/com/tw/CalculatorTest.java
+++ src/test/java/com/tw/CalculatorTest.java
@@ -1,10 +1,12 @@
 package com.tw;
 
 
+import com.tw.exception.DivideByZeroException;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class CalculatorTest {
@@ -15,5 +17,11 @@ class CalculatorTest {
         double result = calculator.calculate("1+1");
 
         MatcherAssert.assertThat(result, is(2.0));
+    }
+
+    @Test
+    void shouldNotDivideByZero() {
+        Calculator calculator = new Calculator();
+        assertThrows(DivideByZeroException.class, () -> calculator.calculate("1/0"));
     }
 }
